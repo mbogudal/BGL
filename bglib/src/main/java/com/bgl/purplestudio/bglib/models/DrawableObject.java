f83357c895061a8066e4d577d2674224d5d8e1ec
mbogudal <mbogudal@icloud.com>
2019-06-26 20:00:00 +0200

    added models

---
+++ bglib/src/main/java/com/bgl/purplestudio/bglib/models/DrawableObject.java
@@ -0,0 +1,8 @@
+package com.bgl.purplestudio.bglib.models;
+
+public class DrawableObject extends Object
+{
+    public int layerId;
+    public double angle;
+    public String path;
+}
---
+++ bglib/src/main/java/com/bgl/purplestudio/bglib/models/Object.java
@@ -0,0 +1,9 @@
+package com.bgl.purplestudio.bglib.models;
+
+public class Object
+{
+    public int posx;
+    public int posy;
+    public int height;
+    public int width;
+}
