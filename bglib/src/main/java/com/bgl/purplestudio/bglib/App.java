60111f06d8b2819acf9bcca4a3e4f2a5d44d1447
mbogudal <mbogudal@icloud.com>
2019-06-26 16:00:35 +0200

    init commit

---
+++ bglib/src/main/java/com/bgl/purplestudio/bglib/App.java
@@ -0,0 +1,11 @@
+package com.bgl.purplestudio.bglib;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+
+public abstract class App {
+    protected Context context;
+    protected Activity activity;
+    protected ConstraintLayout csLayout;
+}
